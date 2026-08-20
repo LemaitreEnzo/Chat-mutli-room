cdda8d48cf7af025bdd216dffc50ebf414baae0c
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2026-08-20 16:09:52 +0200

    Fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
+    cors: true,
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".tsx", ".ts", ".json"],
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
